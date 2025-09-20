da0b432dbfde1342600f437ce227ad82f6cda922
GOWTHAM SAI CHIRIKI <gowthamsai167@gmail.com>
2025-09-21 00:07:02 +0530

    Updated DAO package, by developing the classes UserDao and RoleDao

--- src/main/java/com/authsystem/dao/RoleDao.java
+++ src/main/java/com/authsystem/dao/RoleDao.java
@@ -1,4 +1,45 @@
 package com.authsystem.dao;
 
+import com.authsystem.entity.Role;
+import com.authsystem.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
 public class RoleDao {
+
+    public Role save(Role role) {
+        Transaction tx = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            tx = session.beginTransaction();
+            session.saveOrUpdate(role);
+            tx.commit();
+            return role;
+        } catch (Exception e) {
+            if (tx != null) tx.rollback();
+            throw e;
+        }
+    }
+
+    public Role findById(Long id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.get(Role.class, id);
+        }
+    }
+
+    public Role findByName(String name) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<Role> q = session.createQuery("from Role where name=:n", Role.class);
+            q.setParameter("n", name);
+            return q.uniqueResult();
+        }
+    }
+
+    public List<Role> findAll() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("from Role", Role.class).list();
+        }
+    }
 }
--- src/main/java/com/authsystem/dao/UserDao.java
+++ src/main/java/com/authsystem/dao/UserDao.java
@@ -1,4 +1,64 @@
 package com.authsystem.dao;
 
+import com.authsystem.entity.User;
+import com.authsystem.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
 public class UserDao {
+    public User save(User user) {
+        Transaction tx = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            tx = session.beginTransaction();
+            session.saveOrUpdate(user);
+            tx.commit();
+            return user;
+        } catch (Exception e) {
+            if (tx != null) tx.rollback();
+            throw e;
+        }
+    }
+
+    public User findById(Long id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.get(User.class, id);
+        }
+    }
+
+    public User findByUsername(String username) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<User> q = session.createQuery("from User where username = :u", User.class);
+            q.setParameter("u", username);
+            return q.uniqueResult();
+        }
+    }
+
+    public User findByEmail(String email) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<User> q = session.createQuery("from User where email = :e", User.class);
+            q.setParameter("e", email);
+            return q.uniqueResult();
+        }
+    }
+
+    public List<User> findAll() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("from User", User.class).list();
+        }
+    }
+
+    public void delete(User user) {
+        Transaction tx = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            tx = session.beginTransaction();
+            session.delete(user);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) tx.rollback();
+            throw e;
+        }
+    }
 }
